0565ec11f0c0a20c6bd92963c7800990e2eea72d
JammiK <jammik94@gmail.com>
2018-07-17 23:13:50 +0300

    add joystick

---
+++ Assets/Scripts/UI/Input/MovementJoystick.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Logic.Interfaces;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.UI.Input
+{
+    class MovementJoystick : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _joystick;
+
+        [Inject]
+        private IMouseScreenClickObservable _inputObservable;
+
+        void Start()
+        {
+            _inputObservable.OnMouseScreenClick()
+                .Subscribe(direction =>  _joystick.localPosition = (Vector3) direction * 100);
+        }
+
+    }
+}
